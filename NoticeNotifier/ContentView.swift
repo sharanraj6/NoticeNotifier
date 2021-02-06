db6ff10ba3ce415561a33540a86041639f9026ec
Sharan Raj <sai-sharan-raj.parepalli@capgemini.com>
2021-02-07 00:01:11 +0530

    Initial Commit

---
+++ NoticeNotifier/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoticeNotifier
+//
+//  Created by Sai Sharan Raj Parepalli on 07/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NoticeNotifier/NoticeNotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoticeNotifierApp.swift
+//  NoticeNotifier
+//
+//  Created by Sai Sharan Raj Parepalli on 07/02/21.
+//
+
+import SwiftUI
+
+@main
+struct NoticeNotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
